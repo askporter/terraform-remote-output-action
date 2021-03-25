d149a131e81a165f8e8d97bccfff43950f36716a
George Waters <gwaters92@gmail.com>
2021-03-25 10:05:56 +0000

    Support multiple terraform outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 ORGANIZATION=$1
 WORKSPACE=$2
 TOKEN=$3
-NAME=$4
+NAMES=$4
 
 cat << EOF > $HOME/.terraformrc
 credentials "app.terraform.io" {
@@ -27,4 +27,7 @@ EOF
 
 terraform init
 
-echo "::set-output name=value::$(terraform output $NAME)"
+for name in $NAMES
+do
+    echo "::set-output name=$name::$(terraform output $name)"
+done
