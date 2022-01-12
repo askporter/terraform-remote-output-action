5af5c979782a372fbd094109f09759ed3c979efc
David Mouat Thomson <david.thomson@askproducts.co>
2022-01-12 12:18:10 +0000

    feat: updated action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -e
 
-ORGANIZATION=$1
-WORKSPACE=$2
-TOKEN=$3
-NAMES=$4
+ROLE_ARN=$1
+BUCKET=$2
+BUCKET_KEY=$3
+REGION=$4
+DYNAMODB_TABLE_NAME=$5
+NAMES=$6
 
 cat << EOF > $HOME/.terraformrc
 credentials "app.terraform.io" {
@@ -15,17 +17,21 @@ EOF
 cat << EOF > main.tf
 terraform {
   required_version = ">= 0.14"
-  backend "remote" {
-    organization = "$ORGANIZATION"
-
-    workspaces {
-      name = "$WORKSPACE"
-    }
-  }
+  backend "s3" {}
 }
 EOF
 
-terraform init
+cat << EOF > backend.tfbackend
+role_arn="${ROLE_ARN}"
+bucket = "${BUCKET}"
+key = "${BUCKET_KEY}"
+region = "${REGION}"
+dynamodb_table = "${DYNAMODB_TABLE_NAME}"
+encrypt = true
+
+EOF
+
+terraform init -input=false -backend-config="backend.tfbackend"
 
 for name in $NAMES
 do
